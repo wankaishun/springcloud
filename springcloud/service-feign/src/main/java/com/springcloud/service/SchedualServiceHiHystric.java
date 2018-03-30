c1212de5ad8462a002a5ca4c1cf0416df33deff6
wankaishun <731593846@qq.com>
2018-03-30 17:42:23 +0800

    springcloud 首次提交

---
+++ springcloud/service-feign/src/main/java/com/springcloud/service/SchedualServiceHiHystric.java
@@ -0,0 +1,10 @@
+package com.springcloud.service;
+
+import org.springframework.stereotype.Component;
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
